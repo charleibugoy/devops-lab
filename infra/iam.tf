100a4ffad2696a56acaf7ba3f6e261d2982a1028
Charles Gamboa <charles.y.gamboa@gmail.com>
2026-04-15 10:34:58 -0400

    Update iam.tf

--- infra/iam.tf
+++ infra/iam.tf
@@ -5,5 +5,5 @@ data "aws_iam_role" "eks_cluster" {
 
 # Look up the EKS Node Group Role provided by the KodeKloud environment
 data "aws_iam_role" "eks_node_group" {
-  name = "AmazonEKSNodeRole"
+  name = "AWSServiceRoleForAmazonEKSNodegroup"
 }
