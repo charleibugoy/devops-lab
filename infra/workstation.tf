68a50ce8ed95ef989f4d2883f1d0ec74b6f0761a
Charles Gamboa <charles.y.gamboa@gmail.com>
2026-04-15 09:19:01 -0400

    Update workstation.tf

--- infra/workstation.tf
+++ infra/workstation.tf
@@ -1,19 +1,3 @@
-resource "aws_instance" "workstation" {
-  ami           = data.aws_ami.amazon_linux.id
-  instance_type = "t3.medium"
-
-  subnet_id              = module.vpc.public_subnets[0]
-  vpc_security_group_ids = [aws_security_group.workstation_sg.id]
-
-  associate_public_ip_address = true
-
-  user_data = file("${path.module}/user-data.sh")
-
-  tags = {
-    Name = "devops-workstation"
-  }
-}
-
 resource "aws_key_pair" "workstation" {
   key_name   = "${var.project_name}-workstation-key"
   public_key = var.workstation_public_key
