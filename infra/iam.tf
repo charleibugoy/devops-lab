d6c2e6ec492c167a8c686b7e99b5e7e724182d4c
Charles Gamboa <charles.y.gamboa@gmail.com>
2026-04-15 12:48:13 -0400

    Update iam.tf

--- infra/iam.tf
+++ infra/iam.tf
@@ -8,9 +8,11 @@ data "aws_iam_role" "eks_node_group" {
   name = "AWSServiceRoleForAmazonEKSNodegroup"
 }
 
+/*
 # Create an Instance Profile to wrap the Node Role.
 # This is required to attach the role to an EC2 instance.
 resource "aws_iam_instance_profile" "eks_node_profile" {
   name = "AmazonEKSNodeRole-Profile" # Give it a unique name
   role = data.aws_iam_role.eks_node_group.name
 }
+*/
