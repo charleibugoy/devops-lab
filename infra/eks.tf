c12ae4ca58f22b165303f1cb166031277d6f36c4
Charles Gamboa <charles.y.gamboa@gmail.com>
2026-04-15 10:49:14 -0400

    Update eks.tf

--- infra/eks.tf
+++ infra/eks.tf
@@ -1,30 +1,69 @@
 resource "aws_eks_cluster" "main" {
   name     = var.cluster_name
-  role_arn = data.aws_iam_role.eks_cluster.arn # This now correctly refers to the data source
+  role_arn = data.aws_iam_role.eks_cluster.arn
 
   vpc_config {
     subnet_ids = concat(aws_subnet.public.*.id, aws_subnet.private.*.id)
   }
-
-  # REMOVED: The depends_on block is no longer needed
 }
 
-resource "aws_eks_node_group" "main" {
-  cluster_name    = aws_eks_cluster.main.name
-  node_group_name = "${var.cluster_name}-node-group"
-  node_role_arn   = data.aws_iam_role.eks_node_group.arn # This now correctly refers to the data source
-  subnet_ids      = aws_subnet.private.*.id
-  instance_types  = var.eks_node_instance_types
+# Find the latest EKS-optimized Amazon Linux 2 AMI
+data "aws_ami" "eks_worker" {
+  filter {
+    name   = "name"
+    values = ["amazon-eks-node-${aws_eks_cluster.main.version}-v*"]
+  }
+  most_recent = true
+  owners      = ["602401143452"] # This is the AWS account ID for EKS-optimized AMIs
+}
 
-  scaling_config {
-    desired_size = 2
-    max_size     = 3
-    min_size     = 1
+# The User Data script tells the EC2 instance how to join the cluster
+data "template_file" "worker_userdata" {
+  template = <<-EOF
+    #!/bin/bash
+    set -o xtrace
+    /etc/eks/bootstrap.sh ${aws_eks_cluster.main.name} --b64-cluster-ca '${aws_eks_cluster.main.certificate_authority.0.data}' --apiserver-endpoint '${aws_eks_cluster.main.endpoint}'
+  EOF
+}
+
+# A launch template defines the configuration for our EC2 worker nodes
+resource "aws_launch_template" "eks_worker" {
+  name_prefix = "${var.cluster_name}-worker"
+  image_id    = data.aws_ami.eks_worker.id
+  instance_type = var.eks_node_instance_types[0] # Use the first instance type from the list
+
+  iam_instance_profile {
+    arn = aws_iam_instance_profile.eks_node_profile.arn
   }
 
-  update_config {
-    max_unavailable = 1
+  user_data = base64encode(data.template_file.worker_userdata.rendered)
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "${var.cluster_name}-worker"
+      "kubernetes.io/cluster/${var.cluster_name}" = "owned"
+    }
   }
 
-  # REMOVED: The depends_on block is no longer needed
+  # Ensure the Launch Template is not created until the cluster is ready
+  depends_on = [aws_eks_cluster.main]
+}
+
+# The Auto Scaling Group manages the lifecycle of our worker nodes
+resource "aws_autoscaling_group" "eks_worker" {
+  name_prefix = "${var.cluster_name}-worker-asg"
+  desired_capacity = 2
+  max_size = 3
+  min_size = 1
+  
+  vpc_zone_identifier = aws_subnet.private[*].id
+
+  launch_template {
+    id      = aws_launch_template.eks_worker.id
+    version = "$Latest"
+  }
+
+  # Ensure the ASG doesn't start until the launch template is ready
+  depends_on = [aws_launch_template.eks_worker]
 }
