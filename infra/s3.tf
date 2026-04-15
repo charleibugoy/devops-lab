3c5626cc189815c98eeb93ee6e306d017fce5057
Charles Gamboa <charles.y.gamboa@gmail.com>
2026-04-15 09:16:52 -0400

    Update s3.tf

--- infra/s3.tf
+++ infra/s3.tf
@@ -1,3 +1,22 @@
-resource "aws_s3_bucket" "app" {
-  bucket = "devops-lab-bucket-unique-12345"
+resource "aws_s3_bucket" "main" {
+  bucket = "${var.project_name}-data-${random_string.bucket_suffix.result}"
+
+  tags = {
+    Name = "${var.project_name}-data-bucket"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "main" {
+  bucket = aws_s3_bucket.main.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "random_string" "bucket_suffix" {
+  length  = 8
+  special = false
+  upper   = false
 }
