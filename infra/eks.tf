0f5880adb1bcfced3fcb246f8ddf0e7074d786eb
Charles Gamboa <charles.y.gamboa@gmail.com>
2026-04-15 09:16:33 -0400

    Update eks.tf

--- infra/eks.tf
+++ infra/eks.tf
@@ -1,16 +1,36 @@
-module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
+resource "aws_eks_cluster" "main" {
+  name     = var.cluster_name
+  role_arn = aws_iam_role.eks_cluster.arn
 
-  cluster_name    = "devops-lab"
-  cluster_version = "1.29"
-
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
-
-  eks_managed_node_groups = {
-    default = {
-      instance_types = ["t3.medium"]
-      desired_size   = 2
-    }
+  vpc_config {
+    subnet_ids = concat(aws_subnet.public.*.id, aws_subnet.private.*.id)
   }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_policy,
+  ]
+}
+
+resource "aws_eks_node_group" "main" {
+  cluster_name    = aws_eks_cluster.main.name
+  node_group_name = "${var.cluster_name}-node-group"
+  node_role_arn   = aws_iam_role.eks_node_group.arn
+  subnet_ids      = aws_subnet.private.*.id
+  instance_types  = var.eks_node_instance_types
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_worker_node_policy,
+    aws_iam_role_policy_attachment.eks_cni_policy,
+    aws_iam_role_policy_attachment.ec2_container_registry_read_only,
+  ]
 }
