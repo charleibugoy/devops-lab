3dca7a7d9a4d7d8fc3841e920f6367281551746c
Charles Gamboa <charles.y.gamboa@gmail.com>
2026-04-15 10:52:11 -0400

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5"
+    }
+    github = {
+      source  = "integrations/github"
+      version = "~> 5.0"
+    }
+    template = {
+      source = "hashicorp/template"
+      version = "2.2.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
