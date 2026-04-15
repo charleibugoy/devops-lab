835c12cd96e66643e3937b146c32d14ed4b10ca7
Charles Gamboa <charles.y.gamboa@gmail.com>
2026-04-15 09:15:09 -0400

    Create variable.tf

---
+++ infra/variable.tf
@@ -0,0 +1,84 @@
+# --- General ---
+variable "aws_region" {
+  description = "AWS region for all resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "project_name" {
+  description = "A name for the project, used to prefix resource names"
+  type        = string
+  default     = "my-eks-project"
+}
+
+# --- EKS ---
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+  default     = "my-eks-cluster"
+}
+
+variable "eks_node_instance_types" {
+  description = "Instance types for the EKS node group"
+  type        = list(string)
+  default     = ["t3.medium"]
+}
+
+# --- Networking ---
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+  description = "CIDR blocks for the public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnets" {
+  description = "CIDR blocks for the private subnets"
+  type        = list(string)
+  default     = ["10.0.101.0/24", "10.0.102.0/24"]
+}
+
+# --- ECR ---
+variable "ecr_repository_name" {
+  description = "Name for the ECR container repository"
+  type        = string
+  default     = "my-app-repo"
+}
+
+# --- RDS ---
+variable "db_name" {
+  description = "Name for the RDS database"
+  type        = string
+  default     = "mydatabase"
+}
+
+variable "db_username" {
+  description = "Username for the RDS database master user"
+  type        = string
+  default     = "dbadmin"
+}
+
+variable "db_instance_class" {
+  description = "Instance class for the RDS database"
+  type        = string
+  default     = "db.t3.micro"
+}
+
+# --- Workstation ---
+variable "workstation_instance_type" {
+  description = "Instance type for the EC2 workstation"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "workstation_public_key" {
+  description = "Your public SSH key to access the workstation. Example: 'ssh-rsa AAAA...'"
+  type        = string
+  sensitive   = true
+}
+
