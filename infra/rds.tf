dd4795c2058e632b32355f8201c16d348619f859
Charles Gamboa <charles.y.gamboa@gmail.com>
2026-04-15 09:17:34 -0400

    Update rds.tf

--- infra/rds.tf
+++ infra/rds.tf
@@ -1,8 +1,42 @@
-resource "aws_db_instance" "postgres" {
-  engine            = "postgres"
-  instance_class    = "db.t3.micro"
-  db_name           = "appdb"
-  username          = "admin"
-  password          = "ReplaceMe123!"
-  skip_final_snapshot = true
+resource "aws_db_subnet_group" "main" {
+  name       = "${var.project_name}-rds-subnet-group"
+  subnet_ids = aws_subnet.private.*.id
+
+  tags = {
+    Name = "${var.project_name}-rds-subnet-group"
+  }
+}
+
+resource "random_password" "db_password" {
+  length           = 16
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+
+resource "aws_secretsmanager_secret" "db_password" {
+  name = "${var.project_name}-db-master-password"
+}
+
+resource "aws_secretsmanager_secret_version" "db_password" {
+  secret_id     = aws_secretsmanager_secret.db_password.id
+  secret_string = random_password.db_password.result
+}
+
+resource "aws_db_instance" "main" {
+  identifier           = "${var.project_name}-db"
+  allocated_storage    = 20
+  engine               = "postgres"
+  engine_version       = "15.3"
+  instance_class       = var.db_instance_class
+  db_name              = var.db_name
+  username             = var.db_username
+  password             = random_password.db_password.result
+  db_subnet_group_name = aws_db_subnet_group.main.name
+  vpc_security_group_ids = [aws_security_group.rds.id]
+  skip_final_snapshot  = true
+  publicly_accessible  = false
+
+  tags = {
+    Name = "${var.project_name}-rds-instance"
+  }
 }
