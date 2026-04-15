42462d0a5aa03360b17227e62ab02431fb0e04b4
Charles Gamboa <charles.y.gamboa@gmail.com>
2026-04-15 09:17:09 -0400

    Update ecr.tf

--- infra/ecr.tf
+++ infra/ecr.tf
@@ -1,4 +1,12 @@
-resource "aws_ecr_repository" "app" {
-  name = "devops-lab-app"
-  force_delete = true
+resource "aws_ecr_repository" "main" {
+  name                 = var.ecr_repository_name
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Name = "${var.project_name}-ecr-repo"
+  }
 }
